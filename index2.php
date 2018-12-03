f5fc73ae0c0a6e730641d6a3bb4701053fe9b525
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-03 11:23:19 -0500

    adding the sorted purchases pages

--- index2.php
+++ index2.php
@@ -9,22 +9,22 @@
 ?>
 
 <form action="costasc.php" method="post">
-<?php
-    echo "<h4> Customers: </h4>";
-    $query = "SELECT * FROM customer ORDER BY lastname";
-    $result = mysqli_query($connection, $query);
-    if(!$result){
-        die("database query failed");
-    }
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo '<input type="radio" name = "customer" value = '
-            .$row["cusID"].'>'
-            .$row["firstname"].' '.$row["lastname"];
-        echo "<br>";
-        echo "City: ".$row["city"]."<br>";
-        echo "Phone Number: ".$row["phonenumber"]."<br>"."<br>";
-    }
-    mysqli_free_result($result);
+    <?php
+        echo "<h4> Customers: </h4>";
+        $query = "SELECT * FROM customer ORDER BY lastname";
+        $result = mysqli_query($connection, $query);
+        if(!$result){
+            die("database query failed");
+        }
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<input type="radio" name = "customer" value = '
+                .$row["cusID"].'>'
+                .$row["firstname"].' '.$row["lastname"];
+            echo "<br>";
+            echo "City: ".$row["city"]."<br>";
+            echo "Phone Number: ".$row["phonenumber"]."<br>"."<br>";
+        }
+        mysqli_free_result($result);
     ?>
 <input type="submit" value="Get More information">
 </form> 
@@ -32,12 +32,14 @@
 <br>
 <h2> Add Customer:</h2> <!-- form to add a new customer to the database -->
 <form action="addcustomer.php" method="post" >
-    First Name:     <input type="text" name="firstname"><br>
-    Last Name:      <input type="text" name="lastname"><br>
-    City            <input type="text" name="city"><br>
-    Phone Number:   <input type="text" name="phonenum"><br>
-    
-    <input type="submit" value="Add Customer">
+    <?php
+        echo 'First Name:'.'\t'.'<input type="text" name="firstname"><br>'
+    ?>
+        Last Name:      <input type="text" name="lastname"><br>
+        City:           <input type="text" name="city"><br>
+        Phone Number:   <input type="text" name="phonenum"><br>
+        
+        <input type="submit" value="Add Customer">
 </form>
 
 </body>
