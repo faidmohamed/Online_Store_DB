45a03d3d59f2e606b488245c7d0fa28c49eb1cbd
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-03 13:59:29 -0500

    adding the sorted purchases pages

---
+++ addcustomer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Add Customer</title>
+</head>
+<body>
+<?php
+   include 'connectdb.php';
+?>
+<h1>Here are your pets:</h1>
+<?php
+   $fname=  $_POST["firstname"];
+   $lname = $_POST["lastname"];
+   $city =  $_POST["city"];
+   $city =  $_POST["phonenumber"];
+   $pull_query= 'SELECT MAX(cusID) AS maxid FROM customer';
+   $result=mysqli_query($connection,$pull_query);
+   if (!$result) {
+      die("database max query failed.");
+   }
+   $row=mysqli_fetch_assoc($result);
+   $newkey = intval($row["maxid"]) + 1;
+   $cusID = (string)$newkey;
+   $query = 'INSERT INTO customer VALUES("' . $cusID . '","'. $fname . '","' . $lname . '","' 
+   			. $city  '","' . $phonenumber . '")';
+   if (!mysqli_query($connection, $query)) {
+        die("Error: insert failed" . mysqli_error($connection));
+    }
+   echo "a customer has been added";
+   mysqli_close($connection);
+?>
+</body>
+</html>
