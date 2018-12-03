944e4cf0ef29e66657c78d307163cd0b705e165b
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-03 17:54:21 -0500

    adding the sorted purchases pages

--- addpurchase.php
+++ addpurchase.php
@@ -4,30 +4,37 @@
 <meta charset="utf-8">
 <title>Add Customer</title>
 </head>
-
    <body>
       <?php
          include 'connectdb.php';
       ?>
+      
       <?php
          $cusID =  $_POST["customer"];
          $prodID = $_POST["product"];
          
-         $pull_query = "SELECT quantity FROM purchases WHERE purchases.prodID = '. $prodID . 'AND purchases.cusID = ' .$cusID";
+         $pull_query = 'SELECT quantity FROM purchases WHERE purchases.prodID = '. $prodID . 'AND purchases.cusID = ' .$cusID;
 
          $result = mysqli_query($connection,$pull_query);
          if (!$result) {
-            die("database max query failed.");
+            $query = 'INSERT INTO customer VALUES("' . $cusID . '","' . $prodID . '","'. 1 . '")';
+            if (!mysqli_query($connection, $query)) {
+               die("ERROR: insert failed - " . mysqli_error($connection));
+            }
          }
 
          $row=mysqli_fetch_assoc($result);
          $newQuan = intval($row["quantity"]) + 1;
-         $newQuantity = (string)$newQuan;
-         echo $row;
-         echo '<br>';
-         echo $newQuantity;
-         echo '<br>';
+         $newQuantity = (string)$newkey;
+
+         $query = "UPDATE purchases SET quanitity = " . $newQuantity . " WHERE purchases.prodID = " . $prodID . " AND purchases.cusID = " .$cusID;
+         
+         if (!mysqli_query($connection, $query)) {
+               die("ERROR: insert failed - " . mysqli_error($connection));
+         }
+
+         echo "a purchase has been added";
+         mysqli_close($connection);
       ?>
    </body>
-   
 </html>
