21d3418652de08950ccf9635a88db6a79d47c55e
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-03 16:22:15 -0500

    adding the sorted purchases pages

--- index2.php
+++ index2.php
@@ -30,6 +30,45 @@
 </form> 
 
 <br>
+<h2> Add Purchase:</h2> <!-- form to add a new customer to the database -->
+<form action="addcustomer.php" method="post" >
+    <?php
+            echo "<h4> Customer: </h4>";
+            $query = "SELECT * FROM customer ORDER BY lastname";
+            $result = mysqli_query($connection, $query);
+            if(!$result){
+                die("database query failed");
+            }
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<input type="radio" name = "customer" value = '
+                    .$row["cusID"].'>'
+                    .$row["firstname"].' '.$row["lastname"];
+                echo "<br>";
+            }
+            mysqli_free_result($result);
+        ?>
+    <br><br>
+        <?php
+            echo "<h4> Customer: </h4>";
+            $query = "SELECT * FROM product ORDER BY description";
+            $result = mysqli_query($connection, $query);
+            if(!$result){
+                die("database query failed");
+            }
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<input type="radio" name = "product" value = '
+                    .$row["prodID"].'>'
+                    .$row["description"];
+                echo "<br>";
+            }
+            mysqli_free_result($result);
+        ?>
+    
+    <input type="submit" value="Add Customer">
+</form>
+<br>
+
+
 <h2> Add Customer:</h2> <!-- form to add a new customer to the database -->
 <form action="addcustomer.php" method="post" >
     First Name:     <input type="text" name="firstname"><br>
