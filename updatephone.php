804c74e3e5452091521ca4874811703332b87008
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-04 11:49:01 -0500

    adding the sorted purchases pages

--- updatephone.php
+++ updatephone.php
@@ -23,7 +23,7 @@
           if (!$result) {
                die("database query2 failed.");
            }
-           $row=mysqli_fetch_assoc($result)
+         $row=mysqli_fetch_assoc($result);
          echo $row["firstname"]. " ".$row["lastname"]."'s "."phone number has been updated successfully" ;
          mysqli_close($connection);
       ?>
