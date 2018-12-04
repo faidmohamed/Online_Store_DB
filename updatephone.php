8d18bef796a4bf2b7b436246df2c5105e985e30a
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-04 14:05:12 -0500

    adding the sorted purchases pages

--- updatephone.php
+++ updatephone.php
@@ -12,15 +12,13 @@
       <?php
          $cusID =  $_POST["customer"];
          $newphone = $_POST["newphone"];
-         echo $newphone;
-         echo "<br>";
          $query = 'UPDATE customer SET phonenumber ="' . $newphone . '" WHERE customer.cusID = ' . $cusID;
          echo $query;
          if (!mysqli_query($connection, $query)) {
                die("ERROR2: insert failed - " . mysqli_error($connection));
          }
-      
-         echo "phone number has been updated successfully" ;
+         else      
+            echo "phone number has been updated successfully" ;
          mysqli_close($connection);
       ?>
    </body>
