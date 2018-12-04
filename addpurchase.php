9aa1b367b55e609da3f59d4c5a16f3d707f84a82
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-04 18:46:04 -0500

    adding another piece

--- addpurchase.php
+++ addpurchase.php
@@ -20,7 +20,7 @@
          }
          
          $row = mysqli_fetch_assoc($result);
-         if (!$row) {
+         if ($row["quantity"] == "") {
             $query = 'INSERT INTO purchases values("' . $cusID . '","' . $prodID . '", 1)';
             if (!mysqli_query($connection, $query)) {
                die("ERROR2: insert failed - " . mysqli_error($connection));
