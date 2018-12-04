efe6915fe8184fd630eeaad9ad545e09d625b3f3
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-04 16:43:33 -0500

    adding another piece

--- index2.php
+++ index2.php
@@ -145,7 +145,7 @@ New Phone Number:  <input type="text" name="newphone"><br>
 <h2> Products that have Yet to be Purchased: </h2>
     <ol>
         <?php
-            $query = "SELECT description FROM product WHERE product.description NOT IN (SELECT DISTINCT description FROM customer,product,purchases WHERE customer.cusID = purchases.cusID AND product.prodID = purchases.prodID)"
+            $query = "SELECT description FROM product WHERE product.description NOT IN (SELECT DISTINCT description FROM customer,product,purchases WHERE customer.cusID = purchases.cusID AND product.prodID = purchases.prodID)";
             $result = mysqli_query($connection, $query);
             if(!$result){
                 die("database query failed");
