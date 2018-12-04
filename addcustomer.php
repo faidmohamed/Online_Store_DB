386ae326652b633b3357933b80524c89939e9c91
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-03 19:53:11 -0500

    adding the sorted purchases pages

--- addcustomer.php
+++ addcustomer.php
@@ -31,8 +31,7 @@
    echo '<br>';
    echo $phonenumber;
    echo '<br>';
-
-   $query = 'INSERT INTO customer VALUES("' . $cusID . '","' . $fname . '","'. $lname . '","' . $city . '","' . $phonenumber . '", 11)';
+   $query = 'INSERT INTO customer VALUES("' . $cusID . '","' . $fname . '","'. $lname . '","' . $city . '","' . $phonenumber . '", "11")';
    echo $query;
    echo "<br>";
    if (!mysqli_query($connection, $query)) {
