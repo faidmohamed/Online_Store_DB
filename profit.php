c2e45f9cdc1f776af78d6644d0dfaf5b176fba79
Mohamed Faid <mohamedtfaid@gmail.com>
2018-12-04 17:15:05 -0500

    adding another piece

--- profit.php
+++ profit.php
@@ -12,21 +12,25 @@
       <?php
          $prodID =  $_POST["product"];
          $query = "SELECT SUM(quantity) s FROM purchases WHERE purchases.prodID =" . $prodID;
-         $query2 = "SELECT cost FROM product WHERE product.prodID =" . $prodID;
          $result=mysqli_query($connection,$query);
-         $result2=mysqli_query($connection,$query2);
-         if (!$result || !$reult2) {
-            die("ERROR: " . mysqli_error($connection));
+         if (!$result) {
+            die("ERROR1: " . mysqli_error($connection));
          }
          $row = mysqli_fetch_assoc($result);
-         $row2 = mysqli_fetch_assoc($result2);
          $sum = intval($row["s"]);
+
+         $query2 = "SELECT cost FROM product WHERE product.prodID =" . $prodID;
+         $result2=mysqli_query($connection,$query2);
+         if (!$result2) {
+            die("ERROR2: " . mysqli_error($connection));
+         }         
+         $row2 = mysqli_fetch_assoc($result2);
          $cost = intval($row["cost"]);
+         
          $Rev = $sum*$cost;
          $revenue = (string)$Rev;
          echo "total Revenue is: " . $Revenue;
          mysqli_close($connection);
-
       ?>
 
    <br><br>
